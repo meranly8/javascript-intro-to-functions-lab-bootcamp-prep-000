8adf29d9ce10ab02b42e3747ee4363e4eb7fd8e2
Michelle Ranly <meranly8@gmail.com>
2018-01-11 21:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log('hello').toUpperCase()
+  console.log('hello').toUpperCase
 }
 
 function logWhisper(string) {
-  console.log('HELLO').toLowerCase()
+  console.log('HELLO').toLowerCase
 }
 
